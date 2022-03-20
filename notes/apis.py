27a842e1daf35185e8477ddc5f39db4fa9deb29a
1000007201 <nishant.sharma5504@gmail.com>
2022-03-20 22:43:50 +0530

    [Nishant] Add:SuperUser is added

--- notes/apis.py
+++ notes/apis.py
@@ -1,10 +1,10 @@
 import json
-
 from flask_restful import Resource
 from flask import request, make_response, session, jsonify
 from .validators import validate_add_notes, validate_add_label
 from .utils import token_required
 from .models import Notes
+from user import models
 
 
 class AddNote(Resource):
@@ -24,22 +24,22 @@ class AddNote(Resource):
 
 
 class NotesOperation(Resource):
-    def patch(self, topic):
+    def patch(self, id):
         if not session['logged_in']:
             return {'Error': 'You have to login first'}
         try:
-            note = Notes.objects(topic=topic)
+            note = Notes.objects(id=id)
         except Exception as e:
             return {'Error': str(e)}
         desc = request.form.get('Description')
         note.update(desc=desc)
         return {'message': 'Notes updated'}
 
-    def delete(self, topic):
+    def delete(self, id):
         if not session['logged_in']:
             return {'Error': 'You have to login first'}
         try:
-            note = Notes.objects(topic=topic).first()
+            note = Notes.objects(id=id).first()
         except Exception as e:
             return {'Error': str(e)}
         note.delete()
@@ -87,9 +87,22 @@ class Home(Resource):
     def get(user_name):
         list_notes = []
         try:
-            data_ = Notes.objects(user_name=user_name)
+            data_ = models.Users.objects.filter(user_name=user_name).first()
         except Exception as e:
             return {'Error': str(e)}
+        if data_.is_super_user:
+            dict_all = {}
+            data_user = models.Users.objects()
+            for data in data_user:
+                note = Notes.objects.filter(user_name=data.user_name)
+                list_user = []
+                for itr in note:
+                    dict_itr = {'id': itr.id, 'topic': itr.topic, 'desc': itr.desc, 'color': itr.color, 'label': itr.label}
+                    list_user.append(dict_itr)
+                dict_all[data.user_name] = list_user
+
+            return make_response(dict_all)
+
         for data in data_:
             dict_ = {'id': data.id, 'topic': data.topic, 'desc': data.desc, 'label': data.label}
             list_notes.append(dict_)
--- notes/validators.py
+++ notes/validators.py
@@ -1,12 +1,17 @@
 from .models import Notes
 from flask import session
 
+color_choices = ('red', 'green', 'blue', 'black')
+
 
 def validate_add_notes(body):
     topic = body.get('topic')
     desc = body.get('desc')
     if not desc or not topic:
         return {'Error': 'You have to fill both parameters'}
+    if body.get('color'):
+        if body.get('color') not in color_choices:
+            return {'Error': 'Given color is not valid'}
 
 
 def validate_add_label(body):
